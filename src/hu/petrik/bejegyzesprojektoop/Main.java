b583ff0f01e7559786b0725ab6172628aeeff2c0
Sárándi <sarandi.balint@gmail.com>
2022-09-29 08:46:51 +0200

    2.b: Megadott számú elem hozzáadása listához, érvénválasz: hiba!

--- src/hu/petrik/bejegyzesprojektoop/Main.java
+++ src/hu/petrik/bejegyzesprojektoop/Main.java
@@ -1,17 +1,38 @@
 package hu.petrik.bejegyzesprojektoop;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
+import static java.lang.System.*;
 
 public class Main {
     public static List<Bejegyzes> bejegyzesek = new ArrayList<>();
     public static void main(String[] args) {
         bejegyzesek = new ArrayList<>();
         kettoa();
+        kettob();
     }
 
     public static void kettoa(){
         bejegyzesek.add(new Bejegyzes("Ágozsga", "VisualStudioban javazni"));
         bejegyzesek.add(new Bejegyzes("ZsomBruh", "Profi programozás kulcsa"));
+    }
+
+    public static void kettob(){
+        Scanner sc = new Scanner(in);
+        out.print("Hány új elemet szeretne hozzáadni a listához?\nVálasz: ");
+        try{
+            int darabszam = sc.nextInt();
+            for (int i = 0; i < darabszam; i++) {
+                out.print("Szerző neve: ");
+                String szerzo = sc.next();
+                out.print("Tartalom: ");
+                String tartalom = sc.next();
+                bejegyzesek.add(new Bejegyzes(szerzo,tartalom));
+            }
+        }catch (InputMismatchException e){
+            out.println("Hiba! Nem egész számot adott meg!");
+        }
     }
 }
